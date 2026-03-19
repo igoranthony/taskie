8bc8dc2f811645617f845e06d7555fdd5b115dd6
Igor Anthony <igor.anthony@outlook.com>
2026-03-19 02:07:34 -0300

    fix: adiciona CORS e hosts permitidos no ambiente de desenvolvimento

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.3']
 
 # Database
 DATABASES = {
